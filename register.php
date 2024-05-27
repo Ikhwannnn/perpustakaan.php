7c92404a745af08e0af5539a358a61e2073a5e72
Ikhwan <ikhwanpratamahidayat@gmail.com>
2024-05-27 09:02:06 +0700

    progress pertama

---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Register</title>
+</head>
+<body>
+    <h1>Register</h1>
+    <form action="./register-process.php" method="POST">
+
+    <label for="email">Email : </label><br>
+    <input type="email" id="email" name="email"><br>
+
+    <label for="password">Password :</label><br>
+    <input type="password" id="password" name="password"><br>
+
+    <br>
+    <input type="submit" value="Register"></input>
+</body>
+</html>
